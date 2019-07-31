50be1c78804dfcd8fa7ef84c2e16f946738b5b59
nikolays93 <nikolays93@ya.ru>
2019-07-31 15:44:06 +0400

    Уудалил регистрацию таксономии производителей

--- include/post-types.php
+++ include/post-types.php
@@ -3,36 +3,6 @@
 namespace NikolayS93\Exchange;
 
 function register_post_types() {
-	$developerLabels = array(
-		'name'              => 'Производители',
-		'singular_name'     => 'Производитель',
-		'search_items'      => 'Search brands',
-		'all_items'         => 'All brands',
-		'view_item '        => 'View brands',
-		'parent_item'       => 'Parent brands',
-		'parent_item_colon' => 'Parent brands:',
-		'edit_item'         => 'Edit brands',
-		'update_item'       => 'Update brands',
-		'add_new_item'      => 'Add New brands',
-		'new_item_name'     => 'New brands Name',
-		'menu_name'         => 'Производители',
-	);
-
-	register_taxonomy(
-		apply_filters( 'developerTaxonomySlug', DEFAULT_DEVELOPER_TAX_SLUG ),
-		array( 'product' ),
-		array(
-			'label'       => $developerLabels['name'],
-			'labels'      => $developerLabels,
-			'description' => '', // описание таксономии
-			'public'      => true,
-			// 'publicly_queryable'    => null, // равен аргументу public
-			// 'show_in_nav_menus'     => true, // равен аргументу public
-			// 'show_ui'               => true, // равен аргументу public
-			// 'show_tagcloud'         => true, // равен аргументу show_ui
-		)
-	);
-
 	$warehouseLabels = array(
 		'name'              => 'Склады',
 		'singular_name'     => 'Склад',
